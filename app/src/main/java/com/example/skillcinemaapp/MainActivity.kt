69774e700bb22e4218c66438955b5591e91cb599
Rassul Saduakas <rassaduakas01@gmail.com>
2024-10-26 16:55:12 +0500

    Improved changes

--- app/src/main/java/com/example/skillcinemaapp/MainActivity.kt
+++ app/src/main/java/com/example/skillcinemaapp/MainActivity.kt
@@ -1,20 +1,10 @@
 package com.example.skillcinemaapp
 
 import android.os.Bundle
-import com.example.skillcinemaapp.page.OnBoardingPage
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.skillcinemaapp.graph.RootNavGraph
-import com.example.skillcinemaapp.page.HomePage
-import com.example.skillcinemaapp.ui.theme.SkillCinemaAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
